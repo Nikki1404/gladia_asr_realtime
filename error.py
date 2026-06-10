3978215c47cafa8fe56a259573f69e6790b99fb8
Nikki1404 <81031507+Nikki1404@users.noreply.github.com>
2026-06-10 20:50:25 +0530

    Update error.py

--- error.py
+++ error.py
@@ -9,3 +9,24 @@
   File "/home/nikita_verma2/nemotron_asr/benchmark_maria_nemotron.py", line 99, in run_cmd
     raise RuntimeError(f"Command failed: {' '.join(cmd)}")
 RuntimeError: Command failed: gcloud storage cp gs://cx-asr-test-data/references/maria*_reference.text benchmark_workspace/references
+
+
+gs://cx-asr-test-data/references/herring7_reference.txt
+gs://cx-asr-test-data/references/herring8_reference.txt
+gs://cx-asr-test-data/references/herring9_reference.txt
+gs://cx-asr-test-data/references/maria10_reference.txt
+gs://cx-asr-test-data/references/maria16_reference.txt
+gs://cx-asr-test-data/references/maria18_reference.txt
+gs://cx-asr-test-data/references/maria19_reference.txt
+gs://cx-asr-test-data/references/maria1_reference.txt
+gs://cx-asr-test-data/references/maria20_reference.txt
+gs://cx-asr-test-data/references/maria21_reference.txt
+gs://cx-asr-test-data/references/maria24_reference.txt
+gs://cx-asr-test-data/references/maria27_reference.txt
+gs://cx-asr-test-data/references/maria2_reference.txt
+gs://cx-asr-test-data/references/maria30_reference.txt
+gs://cx-asr-test-data/references/maria31_reference.txt
+gs://cx-asr-test-data/references/maria40_reference.txt
+gs://cx-asr-test-data/references/maria4_reference.txt
+gs://cx-asr-test-data/references/maria7_reference.txt
+gs://cx-asr-test-data/references/sastre10_reference.txt
